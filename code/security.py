020b3cc1b0ab5bf95ff7457969abf2fcadf70350
Ky Huynh <hgky95@gmail.com>
2022-02-23 21:03:07 +0700

    find user by db

--- code/security.py
+++ code/security.py
@@ -5,11 +5,16 @@ users = [User(1, 'bob', 'bob123')]
 username_mapping = {u.username: u for u in users}
 userid_mapping = {u.id: u for u in users}
 
+
 def authenticate(username, password):
-    user = username_mapping.get(username, None)
+    print("authenticate")
+    user = User.find_by_username(username)
     if user and compare_digest(user.password, password):
         return user
 
+
 def identity(payload):
+    print("identity")
     user_id = payload['identity']
-    return userid_mapping.get(user_id, None)
+    print(user_id)
+    return User.find_by_id(100)
