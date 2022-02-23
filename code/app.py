9b1ca24d05ccd332c0fe707bba4c3ceb51a1a173
Ky Huynh <hgky95@gmail.com>
2022-02-23 14:44:49 +0700

    add update api

--- code/app.py
+++ code/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
 from security import authenticate, identity
 
@@ -28,6 +28,21 @@ class Item(Resource):
         global items
         items = list(filter(lambda x: x['name'] == name, items))
         return {'message': f'Item {name} is deleted'}
+
+    def put(self, name):
+        parser = reqparse.RequestParser()
+        parser.add_argument('price',
+                            type=float,
+                            required=True,
+                            help="This field could not be blank")
+        data = parser.parse_args()
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        if item is None:
+            item = {'name': name, 'price': data['price']}
+            items.append(item)
+        else:
+            item.update(data)
+        return item
 
 
 class ItemList(Resource):
