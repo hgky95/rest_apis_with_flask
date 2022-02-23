e4e05262a1d1d28ccdc0c430413cd8cd7323438f
Ky Huynh <hgky95@gmail.com>
2022-02-23 21:03:57 +0700

    add new resource

--- code/app.py
+++ code/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request
 from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
+
+from user import UserRegister
 from security import authenticate, identity
 
 app = Flask(__name__)
@@ -54,5 +56,6 @@ class ItemList(Resource):
 
 api.add_resource(Item, '/items/<string:name>')  # ex: 127.0.0.1:5000/items/chair
 api.add_resource(ItemList, '/items')  # ex: 127.0.0.1:5000/items/chair
+api.add_resource(UserRegister, '/register')
 
 app.run(debug=True)
