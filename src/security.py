9a82f8b5445dd5a8b0a4efce41f6a527420b7a23
Ky Huynh <hgky95@gmail.com>
2022-02-24 14:09:17 +0700

    re-structure the project

--- src/security.py
+++ src/security.py
@@ -1,17 +1,13 @@
-from user import User
+from models.user import UserModel
 from hmac import compare_digest
-
-users = [User(1, 'bob', 'bob123')]
-username_mapping = {u.username: u for u in users}
-userid_mapping = {u.id: u for u in users}
 
 
 def authenticate(username, password):
-    user = User.find_by_username(username)
+    user = UserModel.find_by_username(username)
     if user and compare_digest(user.password, password):
         return user
 
 
 def identity(payload):
     user_id = payload['identity']
-    return User.find_by_id(user_id)
+    return UserModel.find_by_id(user_id)
